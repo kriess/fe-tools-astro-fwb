6efb4766334303152b1f8a6d1d802147d0044f9e
Ken Riess <ken.riess@nfl.com>
2024-12-03 10:30:25 -0800

    feat: npm updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,26 @@
 import { defineConfig } from 'astro/config';
-
+import path from 'path';
+import { fileURLToPath } from 'url';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import node from '@astrojs/node';
 
-const DEV_PORT = 2121;
+const DEV_PORT = 8080;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
-	site: process.env.CI
-		? 'https://themesberg.github.io'
-		: `http://localhost:${DEV_PORT}`,
-	base: process.env.CI ? '/flowbite-astro-admin-dashboard' : undefined,
-
-	// output: 'server',
-
+	output: 'server',
+	adapter: node({
+		mode: 'standalone',
+	}),
 	/* Like Vercel, Netlify,… Mimicking for dev. server */
 	// trailingSlash: 'always',
 
 	server: {
 		/* Dev. server only */
 		port: DEV_PORT,
+		host: true,
 	},
 
 	integrations: [
@@ -27,4 +28,17 @@ export default defineConfig({
 		sitemap(),
 		tailwind(),
 	],
+
+	vite: {
+		base: '',
+		define: {
+			global: {},
+		},
+		publicDir: path.resolve(__dirname, './public'),
+		resolve: {
+			alias: {
+				'~': path.resolve(__dirname, './src'),
+			},
+		},
+	},
 });
